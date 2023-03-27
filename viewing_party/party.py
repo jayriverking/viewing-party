14ac3278d7882f7bc78d7839ec542fadbef7c6ef
Abby <argoodm@gmail.com>
2023-03-27 13:48:33 -0400

    wave2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -95,6 +95,27 @@ print(watch_movie(janes_data, "MOVIE_TITLE_1"))
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+    #Function 1 
+    # Create a function named get_watched_avg_rating. This function should...
+
+    # take one parameter: user_data
+    #     the value of user_data will be a dictionary with a "watched" list of movie dictionaries
+    #         This represents that the user has a list of watched movies
+    # Calculate the average rating of all movies in the watched list
+    #     The average rating of an empty watched list is 0.0
+    # return the average rating
+
+# Function 2 
+
+    # Create a function named get_most_watched_genre. This function should...
+
+    # take one parameter: user_data
+    #     the value of user_data will be a dictionary with a "watched" list of movie dictionaries. Each movie dictionary has a key "genre".
+    #         This represents that the user has a list of watched movies. Each watched movie has a genre.
+    #         The values of "genre" is a string.
+    # Determine which genre is most frequently occurring in the watched list
+    # return the genre that is the most frequently watched
+    # If the value of "watched" is an empty list, get_most_watched_genre should return None
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
