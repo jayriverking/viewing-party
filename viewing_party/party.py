6854f020b735d628691481e45c64eed06aa4e936
Jay King <jayriverking@gmail.com>
2023-03-27 14:45:24 -0400

    wave 3 comments

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -176,6 +176,28 @@ print(get_most_watched_genre(janes_data))
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+# function 1 
+# Create a function named get_unique_watched. This function should...
+# take one parameter: user_data
+# the value of user_data will be a dictionary with a "watched" list of movie dictionaries, and a "friends"
+# This represents that the user has a list of watched movies and a list of friends
+# The value of "friends" is a list
+# Each item in "friends" is a dictionary. This dictionary has a key "watched", which has a list of movie dictionaries.
+# Each movie dictionary has a "title".
+# Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies the user has watched, but none of their friends have watched.
+# Return a list of dictionaries, that represents a list of movies
+
+# function 2
+# Create a function named get_friends_unique_watched. This function should...
+# take one parameter: user_data
+# the value of user_data will be a dictionary with a "watched" list of movie dictionaries, and a "friends"
+# This represents that the user has a list of watched movies and a list of friends
+# The value of "friends" is a list
+# Each item in "friends" is a dictionary. This dictionary has a key "watched", which has a list of movie dictionaries.
+# Each movie dictionary has a "title".
+# Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies at least one of the user's friends have watched, but the user has not watched.
+# Return a list of dictionaries, that represents a list of movies
+
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
