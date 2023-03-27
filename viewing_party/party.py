c96ea56db47a2f7194e29ea624eeb158686690cd
Jay King <jayriverking@gmail.com>
2023-03-27 11:13:49 -0400

    wave 1 comments added

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,6 +2,55 @@
 
 def create_movie(title, genre, rating):
     pass
+# If those three attributes are truthy, then return a dictionary. This dictionary should...
+# Have three key-value pairs, with specific keys
+# The three keys should be "title", "genre", and "rating"
+# The values of these key-value pairs should be appropriate values
+# If title is falsy, genre is falsy, or rating is falsy, this function should return None
+
+# function 2:
+# Create a function named add_to_watched. This function should...
+# take two parameters: user_data, movie
+# the value of user_data will be a dictionary with a key "watched", and a value which is a list of dictionaries representing the movies the user has watched
+# An empty list represents that the user has no movies in their watched list
+# the value of movie will be a dictionary in this format:
+# {
+#   "title": "Title A",
+#   "genre": "Horror",
+#   "rating": 3.5
+# }
+# add the movie to the "watched" list inside of user_data
+# return the user_data
+
+# function 3
+# Create a function named add_to_watchlist. This function should...
+# take two parameters: user_data, movie
+# the value of user_data will be a dictionary with a key "watchlist", and a value which is a list of dictionaries representing the movies the user wants to watch
+# An empty list represents that the user has no movies in their watchlist
+# the value of movie will be a dictionary in this format:
+# {
+#   "title": "Title A",
+#   "genre": "Horror",
+#   "rating": 3.5
+# }
+# add the movie to the "watchlist" list inside of user_data
+# return the user_data
+
+
+# function 4
+# Create a function named watch_movie. This function should...
+# take two parameters: user_data, title
+# the value of user_data will be a dictionary with a "watchlist" and a "watched"
+# This represents that the user has a watchlist and a list of watched movies
+# the value of title will be a string
+# This represents the title of the movie the user has watched
+# If the title is in a movie in the user's watchlist:
+# remove that movie from the watchlist
+# add that movie to watched
+# return the user_data
+# If the title is not a movie in the user's watchlist:
+# return the user_data
+# Note: For Waves 2, 3, 4, and 5, your implementation of each of the functions should not modify user_data.
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
